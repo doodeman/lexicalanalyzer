4b092337bf47fd2ca60b65c7d72c38695ed7c8f5
Kristleifur <batwigg@gmail.com>
2013-09-18 11:55:44 +0000

    Adding token skeleton

---
+++ token.cpp
@@ -0,0 +1,61 @@
+#include "token.h"
+
+Token::Token(void)
+{
+
+}
+
+Token::~Token(void)
+{
+
+}
+
+TokenCode Token::getTokenCode(void) const
+{
+
+}
+
+DataType Token::getDataType(void) const
+{
+
+}
+
+OpType Token::getOpType(void) const
+{
+
+}
+
+SymbolTableEntry* Token::getSymTabEntry(void) const
+{
+
+}
+
+void Token::setTokenCode(const TokenCode tc)
+{
+
+}
+
+void Token::setDataType(const DataType dt)
+{
+
+}
+
+void Token::setOpType(const OpType op)
+{
+
+}
+
+void Token::setSymTabEntry(SymbolTableEntry* entry)
+{
+
+}
+
+std::string Token::opTypeToString(void)
+{
+
+}
+
+std::string Token::tokenCodeToString(void)
+{
+
+}
